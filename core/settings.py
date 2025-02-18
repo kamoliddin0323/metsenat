3fad6be52bf43de860167031220dbf1b6b730612
Пользователь <qutbiddinovkamoliddin797@gmail.com>
2025-02-18 20:06:43 +0500

    last

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = (BASE_DIR / 'staticfiles',)
+
+MEDIA_URL ='media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sponsor/', views.SponsorListCreateAPIView.as_view()),
@@ -12,3 +16,9 @@ urlpatterns = [
     path('graphs/', views.DashboardGraphAPIView.as_view())
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
